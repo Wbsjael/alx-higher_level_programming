8128f41dd881bee830f825edfdf313dc0ce55c48
Wbsjael <muchemijessica77@gmail.com>
2024-06-18 05:16:38 -0700

    javascript objects

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,27 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let j = 0; j < this.height; j++) {
+      let line = '';
+      for (let i = 0; i < this.width; i++) line += 'X';
+      console.log(line);
+    }
+  }
+
+  rotate () {
+    [this.width, this.height] = [this.height, this.width];
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
