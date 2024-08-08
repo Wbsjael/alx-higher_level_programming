9a2737fd8ce3fe6984bc3fbad7c82bb03a6c7d79
Wbsjael <muchemijessica77@gmail.com>
2024-08-08 06:26:04 -0700

    development

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -8,28 +8,20 @@ def matrix_divided(matrix, div):
     div: The divisor.
 
   Returns:
-    The new matrix.
-  """
-
-  if not isinstance(matrix, list):
-      raise TypeError("matrix must be a list of lists")
-
-  for row in matrix:
-      if not isinstance(row, list):
-          raise TypeError("matrix must be a list of lists")
-
-    if len(row) != len(matrix[0]):
-        raise TypeError("Each row of the matrix must have the same size")
-
-  if not isinstance(div, (int, float)):
-      raise TypeError("div must be a number")
-
-  if div == 0:
-      raise ZeroDivisionError("division by zero")
-
-  new_matrix = []
-  for row in matrix:
-      new_row = [round(num / div, 2) for num in row]
-    new_matrix.append(new_row)
-
-  return new_matrix
+    The new matrix."""
+    if not isinstance(matrix, list):
+        raise TypeError("matrix must be a list of lists")
+    for row in matrix:
+        if not isinstance(row, list):
+            raise TypeError("matrix must be a list of lists")
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+    if not isinstance(div, (int, float)):
+        raise TypeError("div must be a number")
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    new_matrix = []
+    for row in matrix:
+        new_row = [round(num / div, 2) for num in row]
+        new_matrix.append(new_row)
+    return new_matrix
